12868625d1ea177f5026a9e121117201563509e4
Gabriel Noriega <gabrielnoriega27@gmail.com>
2020-03-18 13:45:33 -0400

    Added basic comments

--- Assets/Scripts/FSM/Condition.cs
+++
@@ -1,9 +0,0 @@
-namespace Com.StudioTBD.CoronaIO.FMS
-{
-    // public class Condition
-    // {
-    //     public virtual void Run(State to)
-    //     {
-    //     }
-    // }
-}
--- Assets/Scripts/FSM/Extensions.cs
+++ Assets/Scripts/FSM/Extensions.cs
@@ -9,12 +9,5 @@ namespace Com.StudioTBD.CoronaIO.FMS.Extensions
         {
             currentState.StateMachine.ChangeState(newState);
         }
-
-        public static void Transition(this State currentState, State toState,
-            Action<MonoBehaviour> action)
-        {
-            action.Invoke(currentState);
-            ChangeState(currentState, toState);
-        }
     }
 }
