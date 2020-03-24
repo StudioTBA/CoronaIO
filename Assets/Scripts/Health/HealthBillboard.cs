659a1772f1cf66ae7e106837e86cea17d3db9464
Miguel Jimenez <migueleduardo298@hotmail.com>
2020-03-23 20:30:22 -0400

    Health bar will always face the camera
    - New HeathBillboard.cs script that makes the health bar always face the main camera
    - New Scripts/Health folder that contains HealthBar.cs and HealthBillboard.cs

---
+++ Assets/Scripts/Health/HealthBillboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBillboard : MonoBehaviour
+{
+    private GameObject cam;
+
+    private void Awake()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.transform.forward);
+    }
+}
