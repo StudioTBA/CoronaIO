5e844adcd08d56068a87437313930c9f12f810bd
Gabriel Noriega <gabrielnoriega27@gmail.com>
2020-03-23 20:33:12 -0400

    Basic flee

--- Assets/Scripts/Agent/Human/States/Flee.cs
+++ Assets/Scripts/Agent/Human/States/Flee.cs
@@ -30,6 +30,28 @@ namespace Com.StudioTBD.CoronaIO.Agent.Human.States
 
         public override void Execute()
         {
+            if (_dataHolder.Target == null) return;
+
+            // Check radius
+            if (!WithinRadius(transform.position, _dataHolder.Target.transform.position))
+            {
+                _stateMachine.ResetToDefaultState();
+                return;
+            }
+            else
+            {
+                // Run away
+                var position = transform.position;
+                Vector3 dirToPlayer = position - _dataHolder.Target.transform.position;
+                Vector3 newPosition = position + dirToPlayer;
+                _dataHolder.NavMeshAgent.SetDestination(newPosition);
+            }
+
+        }
+
+        private bool WithinRadius(Vector3 currentPosition, Vector3 targetPosition)
+        {
+            return Vector3.Distance(currentPosition, targetPosition) > 50f;
         }
     }
 }
--- Assets/Scripts/Agent/Human/States/Idle.cs
+++ Assets/Scripts/Agent/Human/States/Idle.cs
@@ -9,29 +9,30 @@ namespace Com.StudioTBD.CoronaIO.Agent.Human.States
     public class Idle : State
     {
         private DataHolder _dataHolder;
-        private State _movingState;
+
+        // private State _movingState;
         private State _fleeState;
 
         protected override void Start()
         {
             StateName = "Idle";
             _dataHolder = (StateMachine as HumanStateMachine)?.DataHolder;
-            _movingState = GetComponent<Moving>();
+            // _movingState = GetComponent<Moving>();
             _fleeState = GetComponent<Flee>();
             base.Start();
         }
 
         public override void Execute()
         {
-            if (!Input.GetMouseButtonDown(0)) return;
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            
-            if (!Physics.Raycast(ray, out hit)) return;
-            Vector3 target = new Vector3(hit.point.x, 50f, hit.point.z);
-            _dataHolder.NavMeshAgent.SetDestination(target);
-            this.ChangeState(_movingState);
+            // if (!Input.GetMouseButtonDown(0)) return;
+            //
+            // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // RaycastHit hit;
+            //
+            // if (!Physics.Raycast(ray, out hit)) return;
+            // Vector3 target = new Vector3(hit.point.x, 50f, hit.point.z);
+            // _dataHolder.NavMeshAgent.SetDestination(target);
+            // this.ChangeState(_movingState);
         }
 
         public override void Consume([NotNull] Event.Event @event)
@@ -41,6 +42,7 @@ namespace Com.StudioTBD.CoronaIO.Agent.Human.States
             switch (humanEvent.EventType)
             {
                 case HumanEvent.HumanEventType.SpottedZombie:
+                    this._dataHolder.Target = @event.Producer;
                     this.ChangeState(_fleeState);
                     break;
                 case HumanEvent.HumanEventType.PoliceAlert:
