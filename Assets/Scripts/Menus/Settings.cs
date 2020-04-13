9acdf8ef194321f2973704d6910b16d1185bd2c0
Miguel Jimenez <migueleduardo298@hotmail.com>
2020-04-13 00:17:19 -0400

    Functional settings menu

---
+++ Assets/Scripts/Menus/Settings.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Com.StudioTBD.CoronaIO.MainMenu
+{
+    public class Settings : MonoBehaviour
+    {
+        #region Properties
+
+
+        // Values
+        public int mapScale;
+        public int minSizeToSplit;
+        public int maxNumOfHordes;
+
+        // Sliders
+        public Slider mapScaleSlider;
+        public Slider minSizeToSplitSlider;
+        public Slider maxNumOfHordesSlider;
+
+        // Text
+        public TMP_Text mapScaleText;
+        public TMP_Text minSizeToSplitText;
+        public TMP_Text maxNumOfHordesText;
+
+
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+
+
+        private void Start()
+        {
+            mapScaleSlider.value = mapScale;
+            minSizeToSplitSlider.value = minSizeToSplit;
+            maxNumOfHordesSlider.value = maxNumOfHordes;
+
+            mapScaleText.text = mapScale.ToString();
+            minSizeToSplitText.text = minSizeToSplit.ToString();
+            maxNumOfHordesText.text = maxNumOfHordes.ToString();
+        }
+
+        private void Update()
+        {
+            mapScale = (int) mapScaleSlider.value;
+            minSizeToSplit = (int) minSizeToSplitSlider.value;
+            maxNumOfHordes = (int) maxNumOfHordesSlider.value;
+
+            mapScaleText.text = mapScale.ToString();
+            minSizeToSplitText.text = minSizeToSplit.ToString();
+            maxNumOfHordesText.text = maxNumOfHordes.ToString();
+        }
+
+
+        #endregion
+
+
+        #region Getters
+
+
+        public int GetMapScale()
+        {
+            return mapScale;
+        }
+
+        public int GetMinSizeToSplit()
+        {
+            return minSizeToSplit;
+        }
+
+        public int GetMaxNumOfHordes()
+        {
+            return maxNumOfHordes;
+        }
+
+
+        #endregion
+    }
+}
