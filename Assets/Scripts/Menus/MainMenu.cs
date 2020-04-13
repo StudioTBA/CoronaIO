3c1efee32781e65424914f1f3d285b539aed3cca
Miguel Jimenez <migueleduardo298@hotmail.com>
2020-04-12 21:17:03 -0400

    Settings menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.StudioTBD.CoronaIO.MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        /// <summary>
+        /// Load main game
+        /// </summary>
+        public void LoadGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
