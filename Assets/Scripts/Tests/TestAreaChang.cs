50ffb69bc222397487582efa577b13f8ef2b42cb
Alex <ahayeur@gmail.com>
2020-04-14 21:52:29 -0400

    Adds tactical weighting in the form of "avoid the zombies"

---
+++ Assets/Scripts/Tests/TestAreaChang.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestAreaChang : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Tile")
+        {
+            col.gameObject.GetComponentInChildren<NavMeshModifierVolume>().area=3;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Tile")
+        {
+            col.gameObject.GetComponentInChildren<NavMeshModifierVolume>().area = 0;
+        }
+    }
+}
